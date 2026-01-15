c4e93539eb2e616348b97920b58e79e25ee6542a
Kona Keawekane <konakeawekane@gmail.com>
2026-01-15 13:21:38 -0700

    csharp prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random RNG = new Random();
+        int magicNumber = RNG.Next(1, 100);
+        int answer;
+        String submition = "";
+
+        Console.Write("What is the Magic Number? ");
+        submition = Console.ReadLine();
+        answer = int.Parse(submition);
+        while (!(answer == magicNumber))
+        {
+            if (answer < magicNumber) { Console.WriteLine("Higher"); }
+            if (answer > magicNumber) { Console.WriteLine("Lower"); }
+
+            Console.Write("What is your guess? ");
+            submition = Console.ReadLine();
+            answer = int.Parse(submition);
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
