12f15360795c02fb94581c19e4f429598941348f
Kona Keawekane <konakeawekane@gmail.com>
2026-01-15 13:36:16 -0700

    added replay feature

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,25 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        String submition = "yes";
         Random RNG = new Random();
-        int magicNumber = RNG.Next(1, 100);
+        int magicNumber;
         int answer;
-        String submition = "";
 
-        Console.Write("What is the Magic Number? ");
-        submition = Console.ReadLine();
-        answer = int.Parse(submition);
-        while (!(answer == magicNumber))
-        {
-            if (answer < magicNumber) { Console.WriteLine("Higher"); }
-            if (answer > magicNumber) { Console.WriteLine("Lower"); }
-
-            Console.Write("What is your guess? ");
+        while(submition == "yes")
+        {   
+            magicNumber = RNG.Next(1, 100);
+            Console.Write("What is the Magic Number? ");
             submition = Console.ReadLine();
             answer = int.Parse(submition);
-        }
+            while (!(answer == magicNumber))
+            {
+                if (answer < magicNumber) { Console.WriteLine("Higher"); }
+                if (answer > magicNumber) { Console.WriteLine("Lower"); }
 
-        Console.WriteLine("You guessed it!");
+                Console.Write("What is your guess? ");
+                submition = Console.ReadLine();
+                answer = int.Parse(submition);
+            }
+            Console.WriteLine("You guessed it!");
+            Console.Write("Would you like to play again? ");
+            submition = Console.ReadLine();
+        }
     }
 }
