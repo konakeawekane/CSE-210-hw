6cc913dfdaa07775ff22d2ec1588728e6bb4552b
Kona Keawekane <konakeawekane@gmail.com>
2026-01-15 17:13:03 -0700

    completed csharp prep 4 and 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        Console.Write("Enter number: ");
+        int input = int.Parse(Console.ReadLine());
+
+        while (!(input == 0))
+        {
+            numbers.Add(input);
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+        }
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("You have entered no numbers");
+        } else
+        {
+            int sum = 0;
+            float avg = 0;
+            int largest = numbers[0];
+            int smallestPositive = -1;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+                if (number > largest) { largest = number; }
+                if ((smallestPositive < 0) || (number < smallestPositive) && number > 0) { smallestPositive = number; }
+            }
+
+            avg = sum / numbers.Count;
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {avg}");
+            Console.WriteLine($"The largest number is: {largest}");
+            if (smallestPositive > 0)
+            {
+                Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+            }
+
+            numbers.Sort();
+            Console.WriteLine("The sorted list is:");
+
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+
+        }
+        
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.IO.Pipelines;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int birthYear;
+        DisplayWelcome();
+        String name = PromptUserName();
+        int number = PromptUserNumber();
+        PromtUserBirthYear(out birthYear);
+        number = SquareNumber(number);
+        DisplayResult(name, number, birthYear);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static String PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static void PromtUserBirthYear(out int year)
+    {
+        Console.Write("Please enter your birth year: ");
+        year = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(String name, int number, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+        Console.WriteLine($"{name}, you will turn {DateTime.Now.Year - birthYear} this year.");
+    }
+
 }
